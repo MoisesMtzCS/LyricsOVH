8826be93f9442bc6e6a2ae3820f0aebdfda780d3
Mac <mac@MacBook-Pro-de-Mac-2.local>
2021-02-09 20:06:23 -0600

    First commit

---
+++ app/src/main/java/cs/med/mtz/moises/lyricsovh/api/model/ApiService.kt
@@ -0,0 +1,4 @@
+package cs.med.mtz.moises.lyricsovh.api.model
+
+interface ApiService {
+}
---
+++ app/src/main/java/cs/med/mtz/moises/lyricsovh/domain/MainActivity.kt
@@ -0,0 +1,12 @@
+package cs.med.mtz.moises.lyricsovh.domain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cs.med.mtz.moises.lyricsovh.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
